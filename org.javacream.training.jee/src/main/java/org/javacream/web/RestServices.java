f45b038c4e64be057286f53b4a15ba8f55b15468
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-04 11:15:20 +0200

    introduce simple REST Service

---
+++ org.javacream.training.jee/src/main/java/org/javacream/web/RestServices.java
@@ -0,0 +1,9 @@
+package org.javacream.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestServices extends Application {
+
+}
