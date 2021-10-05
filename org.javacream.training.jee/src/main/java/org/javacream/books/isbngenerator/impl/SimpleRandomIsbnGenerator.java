56822bffffbcd5b5482edd11284321474235923a
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-05 10:24:09 +0200

    add Alternatives as Stereotype

--- org.javacream.training.jee/src/main/java/org/javacream/books/isbngenerator/impl/SimpleRandomIsbnGenerator.java
+++ org.javacream.training.jee/src/main/java/org/javacream/books/isbngenerator/impl/SimpleRandomIsbnGenerator.java
@@ -1,14 +1,12 @@
 package org.javacream.books.isbngenerator.impl;
 
-import java.util.Random;
-
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.javacream.books.isbngenerator.api.IsbnGenerator.RandomStrategy;
-import org.javacream.util.stages.Prod;
+import org.javacream.util.stages.Dev;
 
 @RandomStrategy
-@Prod
-public class RandomIsbnGenerator implements IsbnGenerator {
+@Dev
+public class SimpleRandomIsbnGenerator implements IsbnGenerator {
 
 	private String prefix;
 	private String countryCode;
@@ -19,14 +17,10 @@ public class RandomIsbnGenerator implements IsbnGenerator {
 	public void setCountryCode(String suffix) {
 		this.countryCode = suffix;
 	}
-	private Random random;
 	
-	{
-		random = new Random(this.hashCode() + System.currentTimeMillis());
-	}
 	
 	public String next(){
-		return prefix + random.nextInt() + countryCode;
+		return prefix + Math.random() + countryCode;
 	}
 
 	public String getPrefix(){
