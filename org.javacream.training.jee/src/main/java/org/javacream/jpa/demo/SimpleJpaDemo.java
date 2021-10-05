5b0c0bb1bed2831adcb31b4ca9c31c03a76359e4
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-05 14:30:58 +0200

    first demo native query

--- org.javacream.training.jee/src/main/java/org/javacream/jpa/demo/SimpleJpaDemo.java
+++ org.javacream.training.jee/src/main/java/org/javacream/jpa/demo/SimpleJpaDemo.java
@@ -1,7 +1,10 @@
 package org.javacream.jpa.demo;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -10,7 +13,18 @@ public class SimpleJpaDemo {
 
 	@PersistenceContext private EntityManager entityManager;
 	@GET
+	@Transactional
 	public String doDemo() {
-		return "OK, entityManager=" + entityManager;
+//		//Im Appserver nicht zulässig
+//		EntityTransaction t = entityManager.getTransaction();
+//		t.begin();
+//		//...
+//		t.commit();	
+		
+		entityManager.createNativeQuery("create table messages (col_message varchar(256))").executeUpdate();
+		entityManager.createNativeQuery("insert into messages values('hello')").executeUpdate();
+		List<String> result = entityManager.createNativeQuery("select * from messages").getResultList();
+		entityManager.createNativeQuery("drop table messages").executeUpdate();
+		return "OK, result=" + result;
 	}
 }
