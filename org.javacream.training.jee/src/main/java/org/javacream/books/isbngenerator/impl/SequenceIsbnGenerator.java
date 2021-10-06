7e208a80ac046d5c21fd2cbe58d968a3a71d1ba2
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-06 10:52:54 +0200

    scratch ideas

--- org.javacream.training.jee/src/main/java/org/javacream/books/isbngenerator/impl/SequenceIsbnGenerator.java
+++ org.javacream.training.jee/src/main/java/org/javacream/books/isbngenerator/impl/SequenceIsbnGenerator.java
@@ -6,12 +6,12 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
 import org.javacream.books.isbngenerator.api.IsbnGenerator;
 import org.javacream.logger.DatabaseLogger;
 
-//@Transactional(TxType.REQUIRED) //Default-Attribut
-@Transactional
+@Transactional(TxType.REQUIRED) // Default-Attribut
 public class SequenceIsbnGenerator implements IsbnGenerator {
 
 	@PersistenceContext
@@ -19,7 +19,9 @@ public class SequenceIsbnGenerator implements IsbnGenerator {
 	private String prefix;
 	private String countryCode;
 
-	@Inject private DatabaseLogger databaseLogger;
+	@Inject
+	private DatabaseLogger databaseLogger;
+
 	public String getCountryCode() {
 		return countryCode;
 	}
@@ -31,8 +33,12 @@ public class SequenceIsbnGenerator implements IsbnGenerator {
 	public String next() {
 		Integer actual = (Integer) entityManager.createNativeQuery("select isbn from ISBN").getResultList().get(0);
 		actual++;
-		databaseLogger.log("created isbn " + actual + " at " + new Date());
 		entityManager.createNativeQuery("update ISBN set isbn=" + actual).executeUpdate();
+		//try {
+			databaseLogger.logRequiresNew("created isbn " + actual + " at " + new Date());
+//		} catch (RuntimeException e) {
+//			System.out.println("catched exception from DatabaseLogger");
+//		}
 		return prefix + actual + countryCode;
 	}
 
--- org.javacream.training.jee/src/main/java/org/javacream/logger/DatabaseLogger.java
+++ org.javacream.training.jee/src/main/java/org/javacream/logger/DatabaseLogger.java
@@ -4,14 +4,29 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
 
-@Transactional
 @ApplicationScoped
 public class DatabaseLogger {
 
 	@PersistenceContext private EntityManager entityManager;
 	
-	public void log(String message) {
+	@Transactional(TxType.REQUIRES_NEW)
+	public void logRequiresNew(String message) {
 		entityManager.createNativeQuery("insert into MESSAGES values ('" + message + "')").executeUpdate();
+		//throw new RuntimeException("EXPECTED ROLLBACK");
+	}
+	@Transactional(TxType.REQUIRED)
+	public void logRequired(String message) {
+		entityManager.createNativeQuery("insert into MESSAGES values ('" + message + "')").executeUpdate();
+		//throw new RuntimeException("EXPECTED ROLLBACK");
+	}
+
+	public void log(String message, boolean optional) {
+		if (optional) {
+			logRequiresNew(message);
+		}else {
+			logRequired(message);
+		}
 	}
 }
