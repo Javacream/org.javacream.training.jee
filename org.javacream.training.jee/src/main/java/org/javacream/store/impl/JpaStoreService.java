7b0dc5aaf133da76270f9906042e17f36b6d3dab
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-06 14:19:12 +0200

    finish JpaStoreService

--- org.javacream.training.jee/src/main/java/org/javacream/store/impl/JpaStoreService.java
+++ org.javacream.training.jee/src/main/java/org/javacream/store/impl/JpaStoreService.java
@@ -2,26 +2,33 @@ package org.javacream.store.impl;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
 
 import org.javacream.store.api.StoreService;
+
 @ApplicationScoped
-public class JpaStoreService implements StoreService{
-	
-	@PersistenceContext private EntityManager entityManager;
+public class JpaStoreService implements StoreService {
+
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	@Override
 	@Transactional
 	public int getStock(String category, String item) {
-		Query query = entityManager.createNativeQuery("select * from STORE where category=:cat and item=:item", StoreEntry.class);
-		query.setParameter("cat", category);
-		query.setParameter("item", item);
-		StoreEntry storeEntry = (StoreEntry) query.getSingleResult();
-		return storeEntry.getStock();
+		try {
+			Query query = entityManager.createNativeQuery("select * from STORE where category=:cat and item=:item",
+					StoreEntry.class);
+			query.setParameter("cat", category);
+			query.setParameter("item", item);
+			StoreEntry storeEntry = (StoreEntry) query.getSingleResult();
+			return storeEntry.getStock();
+		} catch (NoResultException e) {
+			System.out.println(e.getMessage());
+			return 0;
+		}
 	}
-	
-	
 
 }
