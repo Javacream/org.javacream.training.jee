9ddff5c129c7366becc3415f14f75f3560540654
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-07 07:26:50 +0200

    add project for relations demo

---
+++ org.javacream.training.jee.relations/src/org/javacream/util/Address.java
@@ -0,0 +1,71 @@
+package org.javacream.util;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	public Address(String city, String street) {
+		super();
+		this.city = city;
+		this.street = street;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [city=" + city + ", street=" + street + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
+	private String city, street;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
