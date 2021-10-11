91f5ba89eea5749ba5f56fb824122c0f97ec1193
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-10-11 11:28:17 +0200

    introduce EchoWebService

---
+++ org.javacream.training.jee/src/main/java/org/javacream/web/RestServices.java
@@ -0,0 +1,11 @@
+package org.javacream.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestServices extends Application{
+	//bleibt bei uns leer: Convention over Configuration
+	
+
+}
