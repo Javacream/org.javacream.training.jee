91dd82ef1eacef739a3161eb9af8edbf88acb8cf
Rainer Sawitzki <rainer.sawitzki@gmail.com>
2025-03-21 08:23:14 +0100

    add OrderWebService

--- projects/org.javacream.training.jee/src/main/java/org/javacream/books/order/web/OrderWebService.java
+++ projects/org.javacream.training.jee/src/main/java/org/javacream/books/order/web/OrderWebService.java
@@ -1,12 +1,19 @@
 package org.javacream.books.order.web;
 
 import jakarta.inject.Inject;
+import jakarta.json.JsonObject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.ExampleObject;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.javacream.books.order.api.Order;
 import org.javacream.books.order.api.OrderService;
-
-import javax.print.attribute.standard.Media;
+import java.util.List;
 
 @Path("orders")
 public class OrderWebService {
@@ -16,5 +23,29 @@ public class OrderWebService {
     @POST @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
     public Order order(OrderCreate orderCreate) {
         return orderService.order(orderCreate.getIsbn(), orderCreate.getNumber());
+    }
+
+    @Path("json")
+    @POST @Produces(MediaType.APPLICATION_JSON) @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "create order")
+    public Order order2(@RequestBody(
+            description = "Order to create", required = true,
+            content = @Content(mediaType = "application/json",
+                    example = "{ \"isbn\": \"ISBN1\", \"title\": \"Title\" }")) JsonObject orderCreate) {
+        return orderService.order(orderCreate.getString("isbn"), orderCreate.getInt("number"));
+    }
+    @GET @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "find all orders")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Created",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Order.class)) }),
+            @APIResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @APIResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
+
+    public List<Order> allOrders() {
+        return orderService.allOrders();
     }
 }
--- projects/org.javacream.training.jee/src/main/java/org/javacream/web/RestServices.java
+++ projects/org.javacream.training.jee/src/main/java/org/javacream/web/RestServices.java
@@ -2,8 +2,18 @@ package org.javacream.web;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
 
 @ApplicationPath("api")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "My APIs",
+                version = "1.0.0",
+                description = "My REST APIs"
+
+        )
+)
 public class RestServices extends Application{
 	//Nichts verpflichtend zu implementieren
 }
